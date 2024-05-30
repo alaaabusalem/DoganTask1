ffc42694c95680624e111391483d32c8954b974c
Ala' Abusalem <23037404@student.ltuc.com>
2024-05-30 21:23:17 +0300

    FinishedJWT

---
+++ CustomersApp/CustomersApp/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using CustomersApp.Models;
+using CustomersApp.Models.DTOs;
+using CustomersApp.Models.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomersApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUser _user;
+        public UsersController(IUser user) {
+        
+            _user = user;   
+        }
+
+        [Route("login")]
+
+        [HttpPost]
+        public  string Create(User user)
+        {
+
+            return  _user.login(user);
+        }
+    }
+}
---
+++ CustomersApp/CustomersApp/Models/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using CustomersApp.Models.Interfaces;
+
+namespace CustomersApp.Models.Services
+{
+    public class UserService:IUser
+    {
+        private readonly JwtTokenService _jwtTokenService;
+
+        public UserService(JwtTokenService jwtTokenService)
+        {
+
+            _jwtTokenService = jwtTokenService; 
+
+        }
+
+        public string login(User user)
+        {
+            if(user.Name!="Admin" || user.Passowrd != "Admin@123")
+            {
+                return "Wrong Name OR Passowred";
+            }
+            string token =  _jwtTokenService.GetToken(user, TimeSpan.FromMinutes(30));
+            return token;
+        }
+    }
+}
